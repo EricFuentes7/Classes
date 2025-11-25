a376544328c914be2ddf086652f41795319bf39b
Eric Fuentes <efuentesgarcia07@gmail.com>
2025-11-25 16:25:32 +0100

    + ex

---
+++ pintar-s-un-art-EricFuentes7/Tasca/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tasca
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fanals = [3, 5, 7, 9, 10];
+            Console.WriteLine($"Fanals: {string.Join(", ", fanals)}");
+            Console.WriteLine($"Solució: {CalculFanals(fanals)}");
+        }
+
+        public static double CalculFanals(int[] fanals){
+            
+            Array.Sort(fanals);
+
+            int anterior = 0;
+            int max = 0;
+            int anteriorFin = 0;
+            foreach (var ubi in fanals)
+            {
+                int diferencia = ubi - anterior;
+                if (diferencia > max)
+                {
+                    max = diferencia;
+                    anteriorFin = ubi;
+                }
+                anterior = ubi;
+            }
+            
+            return Math.Round(anteriorFin - max/2.0);
+            //Console.WriteLine($"Fanals: {string.Join(", ", fanals)}");
+            //Console.WriteLine($"Solució: {Math.Round(anteriorFin - max/2.0)}");
+        }
+    }
+}
---
+++ pintar-s-un-art-EricFuentes7/TascaTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿namespace Tasca;
+public class UnitTest1
+{
+    /*[Fact]
+    public void TestCalculaBePosicio()
+    {
+        int[] fanals = [1,3,7,10,13];
+        var expected = 5;
+
+        var resultat = Program.CalculFanals(fanals);
+
+        Assert.Equal(expected, resultat);
+    }
+    */
+    [Theory]
+    [InlineData(5, 1, 3, 7, 10, 13)]
+    [InlineData(10, 2, 5, 8, 12)]
+    [InlineData(2, 3, 5, 7, 9, 10)]
+    [InlineData(9, 12, 1, 6, 4, 3, 5)]
+    [InlineData(5, 10)]
+
+    
+    public void TestCalculaBePosicio2(int expected, params int[] fanals)
+    {
+        var resultat = Program.CalculFanals(fanals);
+
+        Assert.Equal(expected, resultat);
+    }
+}
